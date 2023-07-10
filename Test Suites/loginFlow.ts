9fc37116dc342bcf759c5eb1dbb8c20244746a91
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-07-10 10:07:11 +0700

    add login flow

---
+++ Test Suites/loginFlow.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>loginFlow</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c99b464c-9574-47eb-ba7d-dd466a1b9a63</testSuiteGuid>
+</TestSuiteEntity>
