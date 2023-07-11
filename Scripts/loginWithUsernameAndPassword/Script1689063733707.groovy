54ef6c01489428cc0a4d3e6027bb27be5d243f38
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-07-11 17:45:51 +0700

    change viewport size

--- Scripts/loginWithUsernameAndPassword/Script1689063733707.groovy
+++ Scripts/loginWithUsernameAndPassword/Script1689063733707.groovy
@@ -29,6 +29,8 @@ WebUI.setEncryptedText(findTestObject('Object Repository/Page_Katalon TestOps/in
 
 WebUI.click(findTestObject('Object Repository/Page_Katalon TestOps/button_Sign in'))
 
+WebUI.setViewPortSize(2880, 1457)
+
 WebUI.takeScreenshotAsCheckpoint('Login Flow Test Run')
 
 WebUI.closeBrowser()
